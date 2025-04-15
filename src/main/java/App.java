3351caea738f7de52d57818d1d36f5119732ffe2
maksim <rv.maks@mail.ru>
2025-04-15 22:24:52 +0300

    https://github.com/MaksPl141/PP-1/tree/Spring_HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println("HelloWorld ссылки одинаковые? " +
+                (bean == bean1));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat ссылки одинаковые? " +
+                (cat1 == cat2));
     }
 }
